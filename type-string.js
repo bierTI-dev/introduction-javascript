f923d7d602a5d232a61294bec948c2640c9da3d6
VictorHBier <victorhbierlbjo@gmail.com>
2021-10-29 20:19:18 -0300

    Exemplos fundamentos do java script

---
+++ type-string.js
@@ -0,0 +1,15 @@
+//type string;
+
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const StringDeNumeros = "34567";
+
+const citacao = "Meu nome é: ";
+const meuNome = "Victor";
+
+//concatenacao (+)
+
+console.log(citacao + meuNome);
+
+//template string OU template literal

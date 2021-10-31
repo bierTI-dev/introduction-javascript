06c4fe411eb023b7269e19b61a36236f3f78b93b
bierTI-dev <victorhbierlbjo@gmail.com>
2021-10-31 17:11:38 -0300

    main

---
+++ conversoes.js
@@ -0,0 +1,27 @@
+//tipo dado
+//booleanos
+
+
+//conversão implicita
+const numero = 456;
+const numeroString = "456";
+
+console.log (numero === numeroString);
+console.log (numero == numeroString);
+
+console.log (numero + numeroString);
+
+
+//conversão explícita
+
+//Number()
+//String()
+
+console.log (numero + Number(numeroString));
+
+
+
+var num1 = 2;
+var num2 = 4;
+
+console.log (num1 + "x" + num2);
---
+++ script.js
@@ -0,0 +1,31 @@
+//var
+
+/*var altura = 5;
+var comprimento = 7;
+
+area = altura * comprimento;
+console.log(area);
+var area;
+
+
+let forma = 'retangulo';
+let altura = 5;
+let comprimento = 7;
+
+if (forma === 'retangulo'){
+    area = altura * comprimento;
+} else{
+    area = (altura * comprimento) / 2;
+}
+
+console.log(area);
+let area;
+
+*/
+
+const forma = 'quadrado';
+const altura = 5;
+const comprimento = 7;
+
+const area = altura * comprimento;
+console.log(area);

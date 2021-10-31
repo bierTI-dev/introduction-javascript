c52a6b98b3025f20a11a4a3668c940548ae4aaee
bierTI-dev <victorhbierlbjo@gmail.com>
2021-10-31 19:04:48 -0300

    Operadores

---
+++ comparacoes.js
@@ -0,0 +1,21 @@
+// == (comparacao implicita)
+
+const numero = 5;
+const texto = "5";
+
+console.log(numero == texto);
+
+// === (comparação explicita)
+
+console.log (numero ===texto);
+
+//typeof
+console.log(typeof numero)
+console.log(typeof texto)
+
+// == so compara o valor
+// === compara o valor e o tipo de dado
+
+//conversao explicita
+Number()
+String()
---
+++ template-strings.js
@@ -0,0 +1,9 @@
+const nome = "Victor";
+const idade = 2021 - 2001;
+const cidadeDeNascimento = "São Leopoldo";
+
+// const apresentacao = "Meu nome é " + nome + ", minha idade é " + idade + " e nasci na cidade de " + cidadeDeNascimento;
+
+const apresentacao = `meu nome é ${nome}, minha idade é ${idade} e nasci na cidade de ${cidadeDeNascimento}`
+
+console.log(apresentacao);

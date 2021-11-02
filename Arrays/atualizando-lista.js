71cf5ab4f1bf72a3f29867de7a348526d40f856e
bierTI-dev <victorhbierlbjo@gmail.com>
2021-11-02 19:50:16 -0300

    Alterando Arrays

---
+++ Arrays/atualizando-lista.js
@@ -0,0 +1,5 @@
+const nomes = ['Victor', 'Maiara', 'Carla', 'Juarez']
+
+//nomes.splice(1, 2, 'Rodrigo')
+nomes.splice(2, 0, 'Rodrigo');
+console.log(nomes)
---
+++ Arrays/lista-duas-dimensoes.js
@@ -0,0 +1,7 @@
+const alunos = ['Victor', 'Maiara', 'Rodrigo', 'Carla', 'Juarez']
+
+const mediaDosAlunos = [10, 7, 9, 6]
+
+let listaDeNotasEAlunos = [alunos, mediaDosAlunos]
+
+console.log(listaDeNotasEAlunos[0][0], listaDeNotasEAlunos[1][0])
